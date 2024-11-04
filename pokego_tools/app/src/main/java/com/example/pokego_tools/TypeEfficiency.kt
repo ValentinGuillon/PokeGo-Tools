4f20e270ab062254aed1dd151e4d1c551fd7c4a2
Sumimasen Valentin-san <valentinkr93430@gmail.com>
2024-11-04 18:10:25 +0100

    progressing...
    place basics layout to mark emplacements

--- pokego_tools/app/src/main/java/com/example/pokego_tools/TypeEfficiency.kt
+++ pokego_tools/app/src/main/java/com/example/pokego_tools/TypeEfficiency.kt
@@ -6,10 +6,14 @@ import android.text.Editable
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 
 class TypeEfficiency : AppCompatActivity() {
+    private var types: MutableList<Boolean> = mutableListOf(false)
+    private lateinit var btnsLayout: LinearLayout
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +21,27 @@ class TypeEfficiency : AppCompatActivity() {
         setContentView(R.layout.type_efficiency)
 
         listenClose()
+        listenTypeBtns()
+    }
+
+    private fun listenTypeBtns() {
+        btnsLayout = findViewById(R.id.btns_layout)
+        val imgsBtns = mutableListOf<ImageButton>()
+
+        for (i in 0 until btnsLayout.childCount) {
+            val child = btnsLayout.getChildAt(i)
+            // Check if the child is a LinearLayout
+            if (child is LinearLayout) {
+                // Loop through the children of the inner LinearLayout
+                for (j in 0 until child.childCount) {
+                    val innerChild = child.getChildAt(j)
+                    // Check if the inner child is an ImageButton and add to list
+                    if (innerChild is ImageButton) {
+                        imgsBtns.add(innerChild)
+                    }
+                }
+            }
+        }
     }
 
     private fun listenClose() {
