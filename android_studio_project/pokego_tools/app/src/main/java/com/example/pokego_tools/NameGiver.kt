4a8a5eeecb1753705609809f64b28ca68933b598
Valentin GUILLON (home) <valentin.guillon93430@gmail.com>
2024-11-09 22:49:34 +0000

    NameGiver: the name no more content the difference between Defense and HP

--- android_studio_project/pokego_tools/app/src/main/java/com/example/pokego_tools/NameGiver.kt
+++ android_studio_project/pokego_tools/app/src/main/java/com/example/pokego_tools/NameGiver.kt
@@ -103,10 +103,9 @@ class NameGiver : AppCompatActivity() {
         val overall = mergeLists(stats, ivs, shadow)
 
         val averageDefHP: Double = (overall[1] + overall[2]) / 2
-        val diffDefHP: String = diffFormated(overall[1], overall[2])
 
 //        val text = "[${statsStr}] + [${ivsStr}] + [${shadow}]"
-        val text = "${overall[0].toInt()}(${ivs[0]})(${ivs[1]})(${ivs[2]})${averageDefHP.toInt()}${diffDefHP}"
+        val text = "${overall[0].toInt()}(${ivs[0]})(${ivs[1]})(${ivs[2]})${averageDefHP.toInt()}"
 
 //        nameLayout.text = "[${stats}] [${ivs}] [${shadow}]"
         nameLayout.text = Editable.Factory.getInstance().newEditable(text)
@@ -126,17 +125,6 @@ class NameGiver : AppCompatActivity() {
 
         return res
 
-    }
-
-
-    private fun diffFormated(a:Double, b:Double) : String {
-        var res = (a - b) / 2
-        var symbol = ">"
-        if (res < 0) {
-            symbol = "<"
-            res = res - res - res
-        }
-        return "${symbol}${res.toInt()}"
     }
 
 
